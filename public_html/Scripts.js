ae6b4e9d5aa2fcdf145429804ff116623dfe6888
Ludvig Basmajyan <lbasmajyan@gmail.com>
2015-01-08 09:21:44 -0800

    Portfolio 3.0 (End of 1/8)
    Changed the color of my text and also changed the background color of some areas. Also added a gif as a background image.

--- public_html/Scripts.js
+++ public_html/Scripts.js
@@ -1,3 +1,29 @@
 $("document").ready(function(){
-
+    $(".col-xs-9").css("color", "blue");
+    
+    $(".col-xs-9").css("background-color", "yellow");
+    
+    $("ul li").css("padding", "6px 140px");
+    
+    $(".col-xs-3").css("background-color", "yellow");
+    
+    $(".col-xs-10").css("background-color", "blue");
+    
+    $(".col-xs-2").css("background-color", "blue");
+    
+    $(".inactive").bind("mouseover", MouseOverMe).bind("mouseout", MouseOutMe);
+    
+    $(".jumbotron").css("color", "lime");
+    
+    $(".jumbotron").css("background-image", "url(https://33.media.tumblr.com/bcf196654e236e37543014e901330c32/tumblr_nbjdcgvVXq1rv6iido1_500.gif)");
 });
+
+function MouseOverMe() 
+{
+    
+}
+
+function MouseOutMe() 
+{
+    
+}
