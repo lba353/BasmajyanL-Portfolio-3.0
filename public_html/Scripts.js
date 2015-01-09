770093fab31374222978c7e04ea406440e25dabc
Ludvig Basmajyan <lbasmajyan@gmail.com>
2015-01-09 09:23:32 -0800

    Portfolio 3.0 (End of 1/9)
    Changed my portfolio a little more and worked on improving the links.

--- public_html/Scripts.js
+++ public_html/Scripts.js
@@ -3,27 +3,39 @@ $("document").ready(function(){
     
     $(".col-xs-9").css("background-color", "yellow");
     
-    $("ul li").css("padding", "6px 140px");
+    $("ul li").css("padding", "6px 150px");
     
     $(".col-xs-3").css("background-color", "yellow");
+    
+    $(".header").css("color", "blue");
     
     $(".col-xs-10").css("background-color", "blue");
     
     $(".col-xs-2").css("background-color", "blue");
     
-    $(".inactive").bind("mouseover", MouseOverMe).bind("mouseout", MouseOutMe);
+    $("li a").bind("mouseover", MouseOverMe).bind("mouseout", MouseOutMe);
     
     $(".jumbotron").css("color", "lime");
     
-    $(".jumbotron").css("background-image", "url(https://33.media.tumblr.com/bcf196654e236e37543014e901330c32/tumblr_nbjdcgvVXq1rv6iido1_500.gif)");
+    //$(".jumbotron").css("background-image", "url(https://33.media.tumblr.com/bcf196654e236e37543014e901330c32/tumblr_nbjdcgvVXq1rv6iido1_500.gif)");
+    
+    $(".h1 ~ h2").css("color", "red");
+    
+    $(".h1").css("color", "red");
+    
+    $(".img-responsive").css("border-style", "solid");
+    
+    $(".img-responsive").css("border-width", "10px");
+    
+    $(".img-responsive").css("border-color", "blue");
 });
 
 function MouseOverMe() 
 {
-    
+    $("li a").css("background-color", "red");
 }
 
 function MouseOutMe() 
 {
-    
+    $("li a").css("background-color", "white");
 }
