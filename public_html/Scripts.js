c711d7079d69e12a8276903ea1ba6a97f47bf6a6
Ludvig Basmajyan <lbasmajyan@gmail.com>
2015-01-12 10:46:46 -0800

    Portfolio 3.0 (End of 1/12)
    Changed, added and removed some HTML elements.

--- public_html/Scripts.js
+++ public_html/Scripts.js
@@ -13,7 +13,11 @@ $("document").ready(function(){
     
     $(".col-xs-2").css("background-color", "blue");
     
-    $("li a").bind("mouseover", MouseOverMe).bind("mouseout", MouseOutMe);
+    $(".inactive + .btn-default").bind("mouseover", MouseOverMe).bind("mouseout", MouseOutMe);
+    
+    $(".dropdown + .btn-default").bind("mouseover", MouseOver).bind("mouseout", MouseOut);
+    
+    $(".active + .btn-default").bind("mouseover", MouseOn).bind("mouseout", MouseOff);
     
     $(".jumbotron").css("color", "lime");
     
@@ -28,14 +32,38 @@ $("document").ready(function(){
     $(".img-responsive").css("border-width", "10px");
     
     $(".img-responsive").css("border-color", "blue");
+    
+    $(".col-xs-2").remove();
+    
+    $(".col-xs-9:last").append("<h1>Projects in Progress</h1>");
 });
 
 function MouseOverMe() 
 {
-    $("li a").css("background-color", "red");
+    $(".inactive + .btn-default").css("background-color", "red");
 }
 
 function MouseOutMe() 
 {
-    $("li a").css("background-color", "white");
+    $(".inactive + .btn-default").css("background-color", "white");
+}
+
+function MouseOver() 
+{
+    $(".dropdown + .btn-default").css("background-color", "red");
+}
+
+function MouseOut() 
+{
+    $(".dropdown + .btn-default").css("background-color", "white");
+}
+
+function MouseOn() 
+{
+    $(".active + .btn-default").css("background-color", "blue");
+}
+
+function MouseOff() 
+{
+    $(".active + .btn-default").css("background-color", "white");
 }
